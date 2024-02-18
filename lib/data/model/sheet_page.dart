bbd0220e64bcea3ea86538685669c948145d2e3a
MS Seo <msseo>
2024-02-19 07:03:48 +0900

    [Model][MusicPage] Add sheetImage member.

--- lib/data/model/sheet_page.dart
+++ lib/data/model/sheet_page.dart
@@ -1,10 +1,14 @@
 
 
+import 'dart:ui';
+
 import 'package:bpm_turner/data/model/sheet_line.dart';
 
+/// Represent page of sheet music.
 class MusicPage {
   final int index;
   final List<Line> lines;
+  final Image? sheetImage;
 
-  MusicPage(this.index, this.lines);
+  MusicPage(this.index, this.lines, {this.sheetImage});
 }
--- lib/data/repository/sheet_repository.dart
+++ lib/data/repository/sheet_repository.dart
@@ -1,14 +1,5 @@
 
-import 'package:bpm_turner/data/service/sheet_database.dart';
-import 'package:bpm_turner/global.dart';
-
-/*
 class SheetRepository {
-  final SheetDatabase _sheetDatabase;
 
-  SheetRepository() :
-    _sheetDatabase = getIt.get<SheetDatabase>();
-
-  void insertSheet() {
-  }
-}*/
+  void insertSheet() {}
+}
