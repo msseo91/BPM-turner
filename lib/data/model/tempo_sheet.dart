b9f24e2b1fb6e3d62373ca756be3156349d8c2f2
MS Seo <msseo>
2024-02-25 15:16:21 +0900

    [Player] Impl of display sheet.

--- lib/data/model/tempo_sheet.dart
+++ lib/data/model/tempo_sheet.dart
@@ -6,10 +6,14 @@ import 'package:equatable/equatable.dart';
 class TempoSheet extends Equatable {
   final String name;
   final List<MusicPage> pages;
+  final int pageIndex;
 
   const TempoSheet(this.name, {
     required this.pages,
+    this.pageIndex = 0,
   });
+
+  MusicPage get currentPage => pages[pageIndex];
 
   @override
   List<Object> get props => [name];
--- lib/feature/player/bloc/player_state.dart
+++ lib/feature/player/bloc/player_state.dart
@@ -1,25 +1,103 @@
 part of 'player_bloc.dart';
 
 sealed class PlayerState extends Equatable {
-  const PlayerState(this.sheet);
+  const PlayerState({
+    required this.sheet,
+    required this.bpm,
+    required this.isMetronome,
+    required this.controlOpacity,
+  });
+
   final TempoSheet sheet;
+  final int bpm;
+  final bool isMetronome;
+  final double controlOpacity;
 
   @override
-  List<Object> get props => [sheet];
+  List<Object> get props => [sheet, bpm, isMetronome, controlOpacity];
 }
 
 final class PlayerInitial extends PlayerState {
-  const PlayerInitial(super.sheet);
+  const PlayerInitial({
+    required super.sheet,
+    required super.bpm,
+    required super.isMetronome,
+    required super.controlOpacity,
+  });
 }
 
 final class PlayerSheetLoaded extends PlayerState {
-  const PlayerSheetLoaded(super.sheet);
+  const PlayerSheetLoaded({
+    required super.sheet,
+    required super.bpm,
+    required super.isMetronome,
+    required super.controlOpacity,
+  });
+
+  PlayerSheetLoaded.fromState(PlayerState state) : super(
+    sheet: state.sheet,
+    bpm: state.bpm,
+    isMetronome: state.isMetronome,
+    controlOpacity: state.controlOpacity,
+  );
 }
 
 final class PlayerRunning extends PlayerState {
-  const PlayerRunning(super.sheet);
+  const PlayerRunning({
+    required super.sheet,
+    required super.bpm,
+    required super.isMetronome,
+    required super.controlOpacity,
+  });
 }
 
 final class PlayerRunComplete extends PlayerState {
-  const PlayerRunComplete(super.sheet);
+  const PlayerRunComplete({
+    required super.sheet,
+    required super.bpm,
+    required super.isMetronome,
+    required super.controlOpacity,
+  });
+}
+
+extension Copy on PlayerState {
+  PlayerState copyWith({
+    TempoSheet? sheet,
+    int? bpm,
+    bool? isMetronome,
+    double? controlOpacity,
+  }) {
+    switch(runtimeType) {
+      case PlayerInitial:
+        return PlayerInitial(
+          sheet: sheet ?? this.sheet,
+          bpm: bpm ?? this.bpm,
+          isMetronome: isMetronome ?? this.isMetronome,
+          controlOpacity: controlOpacity ?? this.controlOpacity,
+        );
+      case PlayerSheetLoaded:
+        return PlayerSheetLoaded(
+          sheet: sheet ?? this.sheet,
+          bpm: bpm ?? this.bpm,
+          isMetronome: isMetronome ?? this.isMetronome,
+          controlOpacity: controlOpacity ?? this.controlOpacity,
+        );
+      case PlayerRunning:
+        return PlayerRunning(
+          sheet: sheet ?? this.sheet,
+          bpm: bpm ?? this.bpm,
+          isMetronome: isMetronome ?? this.isMetronome,
+          controlOpacity: controlOpacity ?? this.controlOpacity,
+        );
+      case PlayerRunComplete:
+        return PlayerRunComplete(
+          sheet: sheet ?? this.sheet,
+          bpm: bpm ?? this.bpm,
+          isMetronome: isMetronome ?? this.isMetronome,
+          controlOpacity: controlOpacity ?? this.controlOpacity,
+        );
+      default:
+        throw StateError('Unknown state: $runtimeType');
+    }
+  }
 }
